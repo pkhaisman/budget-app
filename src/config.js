9a28aaa1ee7774ded230bf7f94e4f17961dfebe7
Philip Khaisman <pkhaisman@gmail.com>
2019-10-22 22:20:07 -0400

    Change API endpoint to access deployed server

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
-    // API_ENDPOINT: 'https://fathomless-citadel-67346.herokuapp.com/api',
+    // API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://fathomless-citadel-67346.herokuapp.com/api',
     API_TOKEN: process.env.REACT_APP_API_TOKEN,
     TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY
 }
